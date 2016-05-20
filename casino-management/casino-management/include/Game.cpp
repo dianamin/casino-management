e05e75c5bd55243e5cc3889a07a69cf484774461
Diana Ghinea <97.diana@gmail.com>
2016-05-20 21:57:20 +0300

    added Game class

---
+++ casino-management/casino-management/include/Game.cpp
@@ -0,0 +1,9 @@
+//
+//  Game.cpp
+//  casino-management
+//
+//  Created by Diana Ghinea on 20/05/16.
+//  Copyright © 2016 Diana Ghinea. All rights reserved.
+//
+
+#include "Game.h"
---
+++ casino-management/casino-management/include/Game.h
@@ -0,0 +1,22 @@
+//
+//  Game.h
+//  casino-management
+//
+//  Created by Diana Ghinea on 20/05/16.
+//  Copyright © 2016 Diana Ghinea. All rights reserved.
+//
+
+#ifndef Game_h
+#define Game_h
+
+#include <stdio.h>
+
+class Game {
+protected:
+    
+    
+public:
+    virtual void play();
+};
+
+#endif /* Game_h */
