8937e71da9e18232c565731cae4cf45debc8d390
valih10 <valentin.harsan10@gmail.com>
2016-05-24 14:52:37 +0300

    Add files via upload

--- casino-management/casino-management/main.cpp
+++ casino-management/casino-management/main.cpp
@@ -9,7 +9,6 @@
 #include "Casino.h"
 #include "Game.h"
 
-
 using namespace std;
 
 int Client :: current_client_id = 0;
@@ -18,25 +17,25 @@ Budget *Budget :: s_instance = 0;
 
 
 int main(int argc, const char * argv[]) {
-    
+
     Casino :: instance() -> set_value(1);
    // cout << Casino :: instance() -> get_value() << '\n';
-    
-    Game g;
-    
+
+    Game *g = new blackjack;
+
     Casino :: instance() -> add_client("Diana");
     cout << '\n';
     (* Casino :: instance() -> get_client(0)).play_game(g);
     (* Casino :: instance() -> get_client(0)).play_game(g);
     (* Casino :: instance() -> get_client(0)).play_game(g);
-    
+
     (* Casino :: instance() -> get_client(0)).get_drink("Apa");
 
-    
+
     Casino :: instance() -> remove_client(0);
-    
-    
+
+
     Budget :: instance() -> show_history();
-    
+
     return 0;
 }
