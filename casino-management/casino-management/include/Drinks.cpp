e4bf405a86d768dd556e4c9f7f96a42b66876bd6
Diana Ghinea <97.diana@gmail.com>
2016-05-25 01:22:34 +0300

    drinks

---
+++ casino-management/casino-management/include/Drinks.cpp
@@ -0,0 +1,9 @@
+//
+//  Drinks.cpp
+//  casino-management
+//
+//  Created by Florin Chirica on 25/05/16.
+//  Copyright © 2016 Florin Chirica. All rights reserved.
+//
+
+#include "Drinks.h"
---
+++ casino-management/casino-management/include/Drinks.h
@@ -0,0 +1,16 @@
+//
+//  Drinks.hpp
+//  casino-management
+//
+//  Created by Florin Chirica on 25/05/16.
+//  Copyright © 2016 Florin Chirica. All rights reserved.
+//
+
+#ifndef Drinks_h
+#define Drinks_h
+
+#include <stdio.h>
+
+
+
+#endif /* Drinks_h */
